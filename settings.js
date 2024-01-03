d0a7860c20c7250ad2c54b1da3251f75993391e3
Fahrul26 <rakrohaku@gmail.com>
2024-01-03 20:50:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Fallxz'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://rakrohaku:Cj7Q5Bd202WvwJfr@cluster0.9h7ssxa.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rakrohaku@gmail.com" //email
+global.email_password = "xnprvzaiaowrooeo" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
